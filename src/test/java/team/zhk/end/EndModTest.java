7a020f7f2f499c0689f6b0c9000cbc8629661ac4
AK47are <3318517070@qq.com>
2025-04-28 17:32:34 +0800

    删除错误的测试类代码。

--- src/test/java/team/zhk/end/EndModTest.java
+++ src/test/java/team/zhk/end/EndModTest.java
@@ -1,14 +1,5 @@
 package team.zhk.end;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
 public class EndModTest {
     
-    @Test
-    public void testOnInitializeNoException() {
-        EndMod mod = new EndMod();
-        assertDoesNotThrow(mod::onInitialize, "onInitialize 方法不应抛出异常");
-    }
 }
